33d136655cb6639a25faf48b0b5f666137dba301
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-17 13:53:57 +0100

    Travis: Run unit tests in CI.
    - Attempt #1
    
     On branch dev/travis
     Changes to be committed:
    	modified:   .travis.yml
    	new file:   bin/install-toolchain.sh
    	modified:   bin/install-verilator.sh
    	modified:   bin/install-yosys.sh

---
+++ bin/install-toolchain.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ -z "$TOOLS_DIR" ]]; then
+    export TOOLS_DIR=./work/tools
+fi
+
+RV_DIR=$TOOLS_DIR/riscv64-unknown-elf
+
+if [[ -z "$RISCV" ]]; then
+    export RISCV=$RV_DIR
+fi
+
+TC_URL=https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14.tar.gz
+TC_FILE=$TOOLS_DIR/toolchain.tar.gz
+
+echo "------------------- Installing Tools --------------------------"
+echo "TOOLS_DIR         '$TOOLS_DIR'"
+echo "RISCV             '$RISCV'"
+echo "Toolchain URL     '$TC_URL'"
+
+set -e
+
+mkdir -p $TOOLS_DIR
+
+if [ ! -e $RISCV/bin/riscv64-unknown-elf-gcc ]; then
+
+    if [ ! -f $TC_FILE ]; then
+
+        echo "Downloading $TC_URL"
+
+        wget -O $TC_FILE $TC_URL
+
+    else
+
+        echo "Toolchain archive already downloaded."
+
+    fi
+
+    echo "Unpacking $TC_FILE"
+
+    tar -xzf $TC_FILE -C $TOOLS_DIR
+
+    mv $RV_DIR-* $RV_DIR
+
+    export RISCV=$RV_DIR
+
+    echo "Toolchain installed to '$RV_DIR'"
+
+else
+
+    echo "Using cached toolchain installation..."
+
+fi
+
+
