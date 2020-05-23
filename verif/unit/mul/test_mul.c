181c9aab836d9cf50ae4b19a6a537da384f82a1a
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-23 21:20:36 +0100

    Verif: Unit: Started test for multiply
     On branch dev/m-ext
     Changes to be committed:
    	modified:   verif/unit/Makefile.in
    	new file:   verif/unit/mul/Makefile.in
    	new file:   verif/unit/mul/test_mul.c
    
     Changes not staged for commit:
    	modified:   rtl/core/core_pipe_exec.sv
    	modified:   rtl/core/core_pipe_exec_mdu.sv

---
+++ verif/unit/mul/test_mul.c
@@ -0,0 +1,28 @@
+
+#include "unit_test.h"
+
+volatile inline int in_mul(int rs1, int rs2) {
+    int     rd;
+    asm volatile("mul %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2));
+    return  rd;
+}
+
+#define CHECK(FN, rs1, rs2) if((rs1*rs2) != FN(rs1,rs2)){test_fail();}
+
+int test_main() {
+
+    CHECK(in_mul,  0,  0);
+    CHECK(in_mul,  1,  0);
+    CHECK(in_mul,  0,  1);
+    CHECK(in_mul,  1,  1);
+    CHECK(in_mul,  2,  1);
+    CHECK(in_mul,  0, -1);
+    CHECK(in_mul, -1,  0);
+    CHECK(in_mul, -1,  1);
+    CHECK(in_mul,  1, -1);
+    CHECK(in_mul, -1, -1);
+
+    return 0;
+
+}
+
