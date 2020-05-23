3d88b90d6086ac30e8fa276b5a02b4753197e2d4
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-23 21:35:10 +0100

    Verif: Unit: Simple mulw test
     On branch dev/m-ext
     Changes to be committed:
    	modified:   verif/unit/mul/test_mul.c
    
     Changes not staged for commit:
    	modified:   rtl/core/core_pipe_exec.sv
    	modified:   rtl/core/core_pipe_exec_mdu.sv

--- verif/unit/mul/test_mul.c
+++ verif/unit/mul/test_mul.c
@@ -1,4 +1,6 @@
 
+#include <stdint.h>
+
 #include "unit_test.h"
 
 volatile inline int in_mul(int rs1, int rs2) {
@@ -7,21 +9,39 @@ volatile inline int in_mul(int rs1, int rs2) {
     return  rd;
 }
 
+volatile inline int32_t in_mulw(int32_t rs1, int32_t rs2) {
+    int     rd;
+    asm volatile("mulw %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2));
+    return  rd;
+}
+
 #define CHECK(FN, rs1, rs2) if((rs1*rs2) != FN(rs1,rs2)){test_fail();}
 
 int test_main() {
 
-    CHECK(in_mul,  0,  0);
-    CHECK(in_mul,  1,  0);
-    CHECK(in_mul,  0,  1);
-    CHECK(in_mul,  1,  1);
-    CHECK(in_mul,  2,  1);
-    CHECK(in_mul,  1,  2);
-    CHECK(in_mul,  0, -1);
-    CHECK(in_mul, -1,  0);
-    CHECK(in_mul, -1,  1);
-    CHECK(in_mul,  1, -1);
-    CHECK(in_mul, -1, -1);
+    CHECK(in_mul   ,  0,  0);
+    CHECK(in_mul   ,  1,  0);
+    CHECK(in_mul   ,  0,  1);
+    CHECK(in_mul   ,  1,  1);
+    CHECK(in_mul   ,  2,  1);
+    CHECK(in_mul   ,  1,  2);
+    CHECK(in_mul   ,  0, -1);
+    CHECK(in_mul   , -1,  0);
+    CHECK(in_mul   , -1,  1);
+    CHECK(in_mul   ,  1, -1);
+    CHECK(in_mul   , -1, -1);
+    
+    CHECK(in_mulw  ,  0,  0);
+    CHECK(in_mulw  ,  1,  0);
+    CHECK(in_mulw  ,  0,  1);
+    CHECK(in_mulw  ,  1,  1);
+    CHECK(in_mulw  ,  2,  1);
+    CHECK(in_mulw  ,  1,  2);
+    CHECK(in_mulw  ,  0, -1);
+    CHECK(in_mulw  , -1,  0);
+    CHECK(in_mulw  , -1,  1);
+    CHECK(in_mulw  ,  1, -1);
+    CHECK(in_mulw  , -1, -1);
 
     return 0;
 
