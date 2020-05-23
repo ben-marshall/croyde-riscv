3775b1f63395af60f79269aa9de2a454304f4097
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-23 22:16:40 +0100

    Verif: Unit: Tests for mulh
     On branch dev/m-ext
     Changes to be committed:
    	modified:   verif/unit/mul/test_mul.c
    
     Changes not staged for commit:
    	modified:   rtl/core/core_pipe_exec.sv
    	modified:   rtl/core/core_pipe_exec_mdu.sv

--- verif/unit/mul/test_mul.c
+++ verif/unit/mul/test_mul.c
@@ -9,39 +9,72 @@ volatile inline int in_mul(int rs1, int rs2) {
     return  rd;
 }
 
+volatile inline int in_mulh(int rs1, int rs2) {
+    int     rd;
+    asm volatile("mulh %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2));
+    return  rd;
+}
+
 volatile inline int32_t in_mulw(int32_t rs1, int32_t rs2) {
     int     rd;
     asm volatile("mulw %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2));
     return  rd;
 }
 
-#define CHECK(FN, rs1, rs2) if((rs1*rs2) != FN(rs1,rs2)){test_fail();}
+#define CHECK_LO(FN, rs1, rs2) {    \
+    int grm    = rs1 * rs2;         \
+    int dut    = FN(rs1,rs2);       \
+    if(grm  !=   dut){              \
+        test_fail();                \
+    }                               \
+}
+
+#define CHECK_HI(FN, rs1, rs2) {    \
+    __int128 result = rs1 * rs2;    \
+      int    grm    = result >> 64; \
+      int    dut    = FN(rs1,rs2);  \
+    if(grm    != dut){              \
+        test_fail();                \
+    }                               \
+}
 
 int test_main() {
 
-    CHECK(in_mul   ,  0,  0);
-    CHECK(in_mul   ,  1,  0);
-    CHECK(in_mul   ,  0,  1);
-    CHECK(in_mul   ,  1,  1);
-    CHECK(in_mul   ,  2,  1);
-    CHECK(in_mul   ,  1,  2);
-    CHECK(in_mul   ,  0, -1);
-    CHECK(in_mul   , -1,  0);
-    CHECK(in_mul   , -1,  1);
-    CHECK(in_mul   ,  1, -1);
-    CHECK(in_mul   , -1, -1);
+    CHECK_LO(in_mul   ,  0,  0);
+    CHECK_LO(in_mul   ,  1,  0);
+    CHECK_LO(in_mul   ,  0,  1);
+    CHECK_LO(in_mul   ,  1,  1);
+    CHECK_LO(in_mul   ,  2,  1);
+    CHECK_LO(in_mul   ,  1,  2);
+    CHECK_LO(in_mul   ,  0, -1);
+    CHECK_LO(in_mul   , -1,  0);
+    CHECK_LO(in_mul   , -1,  1);
+    CHECK_LO(in_mul   ,  1, -1);
+    CHECK_LO(in_mul   , -1, -1);
     
-    CHECK(in_mulw  ,  0,  0);
-    CHECK(in_mulw  ,  1,  0);
-    CHECK(in_mulw  ,  0,  1);
-    CHECK(in_mulw  ,  1,  1);
-    CHECK(in_mulw  ,  2,  1);
-    CHECK(in_mulw  ,  1,  2);
-    CHECK(in_mulw  ,  0, -1);
-    CHECK(in_mulw  , -1,  0);
-    CHECK(in_mulw  , -1,  1);
-    CHECK(in_mulw  ,  1, -1);
-    CHECK(in_mulw  , -1, -1);
+    CHECK_LO(in_mulw  ,  0,  0);
+    CHECK_LO(in_mulw  ,  1,  0);
+    CHECK_LO(in_mulw  ,  0,  1);
+    CHECK_LO(in_mulw  ,  1,  1);
+    CHECK_LO(in_mulw  ,  2,  1);
+    CHECK_LO(in_mulw  ,  1,  2);
+    CHECK_LO(in_mulw  ,  0, -1);
+    CHECK_LO(in_mulw  , -1,  0);
+    CHECK_LO(in_mulw  , -1,  1);
+    CHECK_LO(in_mulw  ,  1, -1);
+    CHECK_LO(in_mulw  , -1, -1);
+    
+    CHECK_HI(in_mulh  ,  0,  0);
+    CHECK_HI(in_mulh  ,  1,  0);
+    CHECK_HI(in_mulh  ,  0,  1);
+    CHECK_HI(in_mulh  ,  1,  1);
+    CHECK_HI(in_mulh  ,  2,  1);
+    CHECK_HI(in_mulh  ,  1,  2);
+    CHECK_HI(in_mulh  ,  0, -1);
+    CHECK_HI(in_mulh  , -1,  0);
+    CHECK_HI(in_mulh  , -1,  1);
+    CHECK_HI(in_mulh  ,  1, -1);
+    CHECK_HI(in_mulh  , -1, -1);
 
     return 0;
 
