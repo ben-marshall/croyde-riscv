e2b3e28dc1b6e817d1db644342b88a8db05546ed
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-31 11:37:25 +0100

    Travis: try adding install symbiyosys script.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   .travis.yml
    	new file:   bin/install-symbiyosys.sh

---
+++ bin/install-symbiyosys.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ -z "$TOOLS_DIR" ]]; then
+    export TOOLS_DIR=./work/tools
+fi
+
+if [[ -z "$SYMBIYOSYS_ROOT" ]]; then
+    export SYMBIYOSYS_ROOT=$TOOLS_DIR/symbiyosys
+fi
+
+SYMBIYOSYS_PATH=$SYMBIYOSYS_ROOT/usr/local/bin
+
+export PATH=$SYMBIYOSYS_PATH:$PATH
+
+SYMBIYOSYS_BIN=$SYMBIYOSYS_PATH/sby
+
+echo "------------------- Installing Tools --------------------------"
+echo "TOOLS_DIR         '$TOOLS_DIR'"
+echo "SYMBIYOSYS_ROOT   '$SYMBIYOSYS_ROOT'"
+echo "PATH              '$PATH'"
+
+set -e
+
+mkdir -p $TOOLS_DIR
+
+if [ ! -e  $SYMBIYOSYS_BIN ]; then
+
+    echo "Building SymbiYosys..."
+    echo
+    echo -en 'travis_fold:start:before_install.symbiyosys\\r'
+    echo
+
+    git clone https://github.com/YosysHQ/SymbiYosys.git $SYMBIYOSYS_ROOT
+
+    cd $SYMBIYOSYS_ROOT
+
+    sudo make install DESTDIR=$SYMBIYOSYS_ROOT
+    
+    echo
+    echo -en 'travis_fold:end:before_install.symbiyosys\\r'
+    echo
+
+    cd -
+
+else
+
+    echo "Using cached SymbiYosys build..."
+
+fi
+
+
+
+
