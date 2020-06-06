60fa5e5ea574601c2293bd0a52e4cf8db8e2174f
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-06-06 11:08:09 +0100

    Verif: Unit: Test to check imem access error
    - imem access error is properly trapped.
    
     On branch dev/phy-addr-width
     Changes to be committed:
    	modified:   verif/unit/Makefile.in
    	modified:   verif/unit/branch-tgt-trap/test_branch_tgt_trap.c
    	new file:   verif/unit/imem-access-trap/Makefile.in
    	new file:   verif/unit/imem-access-trap/test_imem_access_trap.S
    	new file:   verif/unit/imem-access-trap/test_imem_access_trap.c
    
     Changes not staged for commit:
    	modified:   rtl/core/core_pipe_decode.sv
    	modified:   rtl/core/core_pipe_exec.sv
    	modified:   rtl/core/core_top.sv

--- verif/unit/imem-access-trap/test_imem_access_trap.c
+++ verif/unit/imem-access-trap/test_imem_access_trap.c
@@ -11,6 +11,7 @@
     asm volatile ("csrw " #CSR ", %0" : : "r"(rs1));   \
 }
 
+DECL_RD_CSR(mepc)
 DECL_RD_CSR(mcause)
 DECL_RD_CSR(mtvec)
 DECL_WR_CSR(mtvec)
@@ -18,21 +19,36 @@ DECL_WR_CSR(mtvec)
 // Flag set/cleared by c_trap_handler.
 int test_passed = 0;
 
+// Address we will jump to.
+int tgt_addr    = 0;
+
+// Declaration for mtvec target address.
 extern void test_trap_handler();
 
 void c_trap_handler()  {
 
     int64_t mcause = (int64_t)rd_mcause();
+    int64_t mepc   = (int64_t)rd_mepc  ();
 
     if(mcause < 0) {
         // This test should not deal with any interrupts.
         test_fail();
     }
 
-    __putstr("mcause: "); __puthex8(mcause); __putchar('\n');
+    __putstr("mcause: "); __puthex8 (mcause); __putchar('\n');
+    __putstr("mepc  : "); __puthex64(mepc); __putchar('\n');
+
+    if(mepc != 0) {
+        // MEPC should be zero, since this is the faulting address that
+        // we jumped too.
+        test_fail();
+    }
 
     if(mcause == CAUSE_CODE_IACCESS) {
-        test_passed += 1;
+        // Pass immediately, since we cannot recover from this
+        // kind of trap. mepc is written with the target of the
+        // jump, rather than the instruction which caused the jump.
+        test_pass();
     } else {
         test_fail();
     }
@@ -45,8 +61,7 @@ int test_main() {
     // Set MTVEC to the trap handler.
     wr_mtvec((uint64_t)(&test_trap_handler));
 
-    // Jump to a non-existant physical address. -1 in this case.
-    int tgt_addr = -1;
+    // Jump to a non-existant physical address. 0 in this case.
     asm ("jr %0" : : "r"(tgt_addr));
 
     // Check we saw the exception handler only once.
