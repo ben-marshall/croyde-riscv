ben-marshall/croyde-riscv

commit 0c018a5fae787f26785d52ee848243f5288f042c

rtl/core: Remove feedthrough path imem_gnt -> dmem_req
- dmem_req did not need to depend on imem_gnt

 On branch dev/ccx
 Changes to be committed:
	modified:   rtl/core/core_top.sv

 Changes not staged for commit:
	modified:   flow/verilator/Makefile.in
	modified:   rtl/ccx/ccx_ic_arbiter.sv
	modified:   rtl/ccx/ccx_ic_router.sv
	modified:   rtl/ccx/ccx_ic_top.sv
	modified:   rtl/ccx/ccx_top.sv
	modified:   rtl/mem/mem_sram_wxd.v

 Untracked files:
	flow/verilator/cmd-ccx.txt
	flow/verilator/manifest-rtl-ccx.txt
	flow/verilator/manifest-tb-ccx.txt
	verif/ccx/

rtl/core/core_top.sv
@@ -77,7 +77,7 @@ wire [ CF_CAUSE_R:0] int_cause   ; // Cause code for the interrupt.
 wire [         XL:0] int_tvec    ; // Interrupt trap vector
 wire                 int_ack     ; // Interrupt taken acknowledge
 
-wire                 s2_flush    = s3_cf_valid && s3_cf_ack;
+wire                 s2_flush    = s3_cf_valid;
 
 //
 // Inter-stage wiring

commit 32252621591f270337da58b2c0bfe9aa6da84bd7

rtl/core: Fix use of s2_flush with LSU
- Fix bug introduced in 0c018a5 which fixed a feed-through path, but
  also caused delayed branches in writeback to be cancelled.

 On branch dev/ccx
 Changes to be committed:
	modified:   rtl/core/core_pipe_exec.sv
	modified:   rtl/core/core_top.sv

 Changes not staged for commit:
	modified:   .travis.yml
	modified:   flow/verilator/Makefile.in

 Untracked files:
	flow/verilator/cmd-ccx.txt
	flow/verilator/manifest-rtl-ccx.txt
	flow/verilator/manifest-tb-ccx.txt
	verif/ccx/

rtl/core/core_pipe_exec.sv
@@ -14,6 +14,7 @@ input  wire                 s2_cf_ack       , // Control flow acknwoledged
 output wire [         XL:0] s2_cf_target    , // Control flow destination
 
 input  wire                 s2_flush        , // Flush pipestage contents
+input  wire                 s2_cancel       , // Stop S2 instrs doing stuff.
 input  wire [         XL:0] csr_mepc        , // return address for mret
 
 output wire                 s2_ready        , // EX ready for new instruction
@@ -207,7 +208,7 @@ wire                 cfu_finished    ; // CFU instruction finished.
 // LSU interfacing
 // ------------------------------------------------------------
 
-wire        lsu_valid       =  s2_valid && !s3_trap && !s2_flush &&
+wire        lsu_valid       =  s2_valid && !s3_trap && !s2_cancel&&
                               (s2_lsu_load || s2_lsu_store);
 wire        lsu_ready       ;
 wire        lsu_trap_addr   ;

rtl/core/core_top.sv
@@ -77,7 +77,8 @@ wire [ CF_CAUSE_R:0] int_cause   ; // Cause code for the interrupt.
 wire [         XL:0] int_tvec    ; // Interrupt trap vector
 wire                 int_ack     ; // Interrupt taken acknowledge
 
-wire                 s2_flush    = s3_cf_valid;
+wire                 s2_flush    = s3_cf_valid && s3_cf_ack;
+wire                 s2_cancel   = s3_cf_valid ;
 
 //
 // Inter-stage wiring
@@ -428,6 +429,7 @@ core_pipe_exec #(
 .s2_cf_ack       (s2_cf_ack       ), // Control flow acknwoledged
 .s2_cf_target    (s2_cf_target    ), // Control flow destination
 .s2_flush        (s2_flush        ), // Flush stage contents.
+.s2_cancel       (s2_cancel       ), // Stage 1 flush
 .csr_mepc        (csr_mepc        ), // MRET return address
 .s2_ready        (s2_ready        ), // EX ready for new instruction
 .s2_valid        (s2_valid        ), // Decode -> EX instr valid.