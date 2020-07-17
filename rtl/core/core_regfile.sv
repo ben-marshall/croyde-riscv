95a5e1e7906bc4a3a7e778a37b43628663b348f9
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-07-17 21:52:17 +0100

    rtl/core: BRAM friendly regfile

--- rtl/core/core_regfile.sv
+++ rtl/core/core_regfile.sv
@@ -26,28 +26,17 @@ input  wire [        XL:0] rd_wdata
 // Common parameters and width definitions.
 `include "core_common.svh"
 
-wire [XL:0] regs[31:0];
+reg  [XL:0] regs  [31:0];
 
-assign  rs1_data    = regs[rs1_addr];
-assign  rs2_data    = regs[rs2_addr];
+assign  rs1_data    = |rs1_addr ? regs[rs1_addr] : {XLEN{1'b0}};
+assign  rs2_data    = |rs2_addr ? regs[rs2_addr] : {XLEN{1'b0}};
 
 assign  g_clk_req   = rd_wen;
 
-assign regs[0]      = 0;
-
-genvar i;
-generate for(i = 1; i < 32; i = i + 1) begin
-
-    reg [XL:0] r;
-
-    assign regs[i] = r;
-
-    always @(posedge g_clk) begin
-        if(rd_wen && (rd_addr == i)) begin
-            r <= rd_wdata;
-        end
+always @(posedge g_clk) begin
+    if(rd_wen && |rd_addr) begin
+        regs[rd_addr] <= rd_wdata;
     end
-
-end endgenerate
+end
 
 endmodule
