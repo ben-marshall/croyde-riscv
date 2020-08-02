dde3c9a45bd804cc3510e1f9c4732188627fcb98
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-02 19:15:38 +0100

    src/csp: cosmetics

--- src/csp/uc64_csp.h
+++ src/csp/uc64_csp.h
@@ -7,20 +7,20 @@
 uint64_t volatile * const uc64_csp_mtime    ;
 uint64_t volatile * const uc64_csp_mtimecmp ;
 
-inline uint64_t uc64_csp_rd_mtime() {
-    return uc64_csp_mtime[0];
+inline volatile uint64_t uc64_csp_rd_mtime() {
+    return *uc64_csp_mtime;
 }
 
-inline uint64_t uc64_csp_rd_mtimecmp() {
-    return uc64_csp_mtimecmp[0];
+inline volatile uint64_t uc64_csp_rd_mtimecmp() {
+    return *uc64_csp_mtimecmp;
 }
 
 inline void uc64_csp_wr_mtime(uint64_t new_value) {
-    uc64_csp_mtime[0] = new_value;
+    *uc64_csp_mtime = new_value;
 }
 
 inline void uc64_csp_wr_mtimecmp(uint64_t new_value) {
-    uc64_csp_mtimecmp[0] = new_value;
+    *uc64_csp_mtimecmp = new_value;
 }
 
 inline          void     uc64_csp_wfi() {
