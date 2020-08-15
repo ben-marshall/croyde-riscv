89ff04e37f5b417ae15a2a81701028efe0a67064
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-15 11:39:20 +0100

    flow: Fix reading of core SV source files.
    - Use single TCL script for designer assertions and riscv-formal
    
     On branch dev/pmp
     Changes to be committed:
    	modified:   flow/design-assertions/core_fetch_buffer.sby
    	new file:   flow/design-assertions/read_core_rtl.tcl
    	modified:   flow/riscv-formal/checks.cfg
    
     Changes not staged for commit:
    	modified:   extern/embench-iot (modified content)

---
+++ flow/design-assertions/read_core_rtl.tcl
@@ -0,0 +1,20 @@
+yosys -import
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/prim/prim_clock_gate.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_clock_ctrl.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_csrs_if.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_fetch_buffer.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_fetch.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_decode_immediates.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_decode.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_exec_alu.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_exec_cfu.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_exec_lsu.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_exec_mdu.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_exec.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pipe_wb.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_csrs.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_pmp.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_regfile.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_interrupts.sv
+read_verilog -sv -formal $::env(REPO_HOME)/rtl/core/core_top.sv
+
